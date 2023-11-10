dcd550b0e74ad8968ac92089c41bf5b1c958be02
Zaaraa96 <zahraahmadi96@gmail.com>
2023-11-10 18:49:29 +0330

    create user and report module

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(
+      {
+        type: 'sqlite',
+        database: 'mytest',
+        entities:[],
+        synchronize: true,
+      }
+    ),
+    UsersModule, ReportsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
